68a6528b8d1708bcdca6f25c621d15dec7c066f5
kishore007k <kishorekumarstar07@gmail.com>
2020-10-23 21:16:59 +0530

    Added mockData
    Signed-off-by: kishore007k <kishorekumarstar07@gmail.com>

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = "https://api.github.com";
+
+const GitHubContext = React.createContext();
+
+const GitHubProvider = ({ children }) => {
+	return (
+		<GitHubContext.Provider value={"hello"}>{children}</GitHubContext.Provider>
+	);
+};
+
+export { GitHubContext, GitHubProvider };
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,21 +1,34 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-const Error = () => {
-  return <h2>error page</h2>;
-};
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
 const Wrapper = styled.section`
-  min-height: 100vh;
-  display: grid;
-  place-items: center;
-  background: var(--clr-primary-10);
-  text-align: center;
-  h1 {
-    font-size: 10rem;
-  }
-  h3 {
-    color: var(--clr-grey-3);
-    margin-bottom: 1.5rem;
-  }
+	min-height: 100vh;
+	display: grid;
+	place-items: center;
+	background: var(--clr-primary-10);
+	text-align: center;
+	h1 {
+		font-size: 10rem;
+	}
+	h3 {
+		color: var(--clr-grey-3);
+		margin-bottom: 1.5rem;
+	}
 `;
+
+const Error = () => {
+	return (
+		<Wrapper>
+			<div>
+				<h1>404</h1>
+				<h3>Sorry, the page you tried reaching is not available</h3>
+				<Link to="/" className="btn">
+					Home Page
+				</Link>
+			</div>
+		</Wrapper>
+	);
+};
+
 export default Error;
